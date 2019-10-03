540186c3ba4f03efe2fce7ead07ef558611df80c
JetsonHacksNano <jim@jetsonhacks.com>
2019-10-02 21:07:33 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # installArduinoIDE
 Install the Arduino IDE on the NVIDIA Jetson Developer Kit.
+
+Consider donating to Arduino development at: https://www.arduino.cc/
+
+As of this writing (October 2019) the Arduino IDE in the Ubuntu ARM 64 repository does not support the Jetson. This script downloads a version which supports the Jetson and installs it. To run the script:
+
+$ ./installArduinoIDE.sh
+
+<h3>Notes</h3>
+
+Initial Release October, 2019
+* L4T 32.2.1 / JetPack 4.2.2
+* Installs Arduino IDE 1.8.10
---
+++ installArduinoIDE.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright (c) 2016-19 Jetsonhacks 
+# MIT License
+# Download and install the Arduino IDE
+# This is a *very* simple script, change the environment 
+# variables to your liking
+
+INSTALL_DIR=${HOME}
+# Direct Jetson support starts at 1.8.10
+ARDUINO_VERSION=1.8.10
+
+# Only download if newer version exists
+wget -N https://downloads.arduino.cc/arduino-$ARDUINO_VERSION-linuxaarch64.tar.xz
+tar -C $INSTALL_DIR/ -xvf arduino-${ARDUINO_VERSION}-linuxaarch64.tar.xz
+cd $INSTALL_DIR/arduino-${ARDUINO_VERSION}
+sudo ./install.sh
+sudo ./arduino-linux-setup.sh $USER
+echo "You can delete the tar file if desired: arduino-"${ARDUINO_VERSION}"-linuxaarch64.tar.xz"
